e93b884dfe8a768c5ceb770e81e9eb6ae8d691fe
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-04-27 01:17:49 +0000

    feat: añadir test para la clase Libro

--- sistemaGestionBiblioteca/src/test/java/app/biblioteca/recursos/LibroTest.java
+++ sistemaGestionBiblioteca/src/test/java/app/biblioteca/recursos/LibroTest.java
@@ -1,4 +1,20 @@
 package app.biblioteca.recursos;
 
+import app.biblioteca.utils.Estado;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LibroTest {
+
+    @Test
+    void testCrearLibroValido() {
+        // Crear un libro valido
+        Libro libro = new Libro ("978-3-16-148410-0", "Clean Code", "Robert C. Martin");
+
+        //Verificar que los valores se asignen correctamente
+        assertEquals("978-3-16-148410-0", libro.getIsbn());
+        assertEquals("Clean Code", libro.getTitulo());
+        assertEquals("Robert C. Martin", libro.getAutor());
+        assertEquals(Estado.DISPONIBLE, libro.getEstado());
+    }
 }
